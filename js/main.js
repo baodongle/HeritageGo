b163634a3141d3f49410cfd25834a7afc70d59e0
baodongle <baodongle@gmail.com>
2019-06-25 19:11:16 +0700

    Waypoints 14-15
    - Waypoint 14: Design Photo Post Layout
    - Waypoint 15: Design Feed Layout

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 // Toggle Password Visibility
-$(".toggle-password").click(function () {
-    $(this).toggleClass("fa-lock fa-unlock-alt");
-    let input = $("#defaultForm-pass");
-    if (input.attr("type") === "password") {
-        input.attr("type", "text");
-    } else {
-        input.attr("type", "password");
-    }
-});
+$('.toggle-password').click(function () {
+  let input = $('#defaultForm-pass')
+  $(this).toggleClass('fa-lock fa-unlock-alt')
+  if (input.attr('type') === 'password') {
+    input.attr('type', 'text')
+  } else {
+    input.attr('type', 'password')
+  }
+})
+
+// Submit post comment without reload the page
+$('.post__comment').submit(function () {
+  // TODO: submit comment
+  return false
+})

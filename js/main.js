d4e6d507dcd741847602171ebf46f9a92ed009de
baodongle <baodongle@gmail.com>
2019-06-29 10:26:35 +0700

    Waypoint 18

--- js/main.js
+++ js/main.js
@@ -6,28 +6,27 @@ function renderPhoto (image, photoPost, mainContent) {
   mHeritageGoService
     .getPhoto(image)
     .then(photo => {
-      let newPost = photoPost.clone()
-      newPost
+      photoPost
         .find('.post__avatar')
-        .attr('src', 'http:' + photo.account.picture_url)
-      newPost.find('.post__caption').html(photo.title[0].content)
-      newPost.find('.post__area-name').html(photo.area_name)
-      newPost
+        .attr('src', `http:${photo.account.picture_url}`)
+      photoPost.find('.post__caption').html(photo.title[0].content)
+      photoPost.find('.post__area-name').html(photo.area_name)
+      photoPost
         .find('.post__photo')
-        .attr('src', 'http:' + photo.image_url + '?size=medium')
+        .attr({
+          'src': `http:${photo.image_url}?size=medium`,
+          'alt': photo.title[0].content
+        })
       if (photo.capture_time) {
-        newPost.find('.post__date').find('span').html(photo.capture_time)
+        photoPost.find('.post__date').find('span').html(photo.capture_time)
       } else {
-        newPost.find('.post__date').remove()
+        photoPost.find('.post__date').remove()
       }
-      newPost.find('.post__like-count').html(photo.like_count)
-      newPost.find('.post__comment-count').html(photo.comment_count)
-      newPost.find('.post__view-count').html(photo.view_count)
+      photoPost.find('.post__like-count').html(photo.like_count)
+      photoPost.find('.post__comment-count').html(photo.comment_count)
+      photoPost.find('.post__view-count').html(photo.view_count)
 
-      newPost.removeAttr('id')
-      newPost.removeAttr('hidden')
-
-      newPost.hide().appendTo(mainContent).fadeIn()
+      photoPost.hide().appendTo(mainContent).fadeIn()
     })
     .catch(error => {
       console.log(error)
@@ -42,10 +41,13 @@ function renderPhotos (ignored) {
       offset: ignored
     })
     .then(photos => {
-      let post = $('#defaultPost')
+      let postId = $('#defaultPost')
       isLoading = false
       $(photos).each(function () {
-        renderPhoto(this, post, main)
+        let newPost = postId.clone()
+        newPost.removeAttr('id')
+        newPost.removeAttr('hidden')
+        renderPhoto(this, newPost, main)
       })
     })
     .catch(error => {
@@ -54,10 +56,17 @@ function renderPhotos (ignored) {
 }
 
 $(function () {
-  let ignored = 2
+  let ignored = 3
+  const clonedMain = $('main').clone()
+  clonedMain.css('padding-top', $('header').height() + 20)
+  $('.blur-background').append(clonedMain)
   renderPhotos(ignored)
   isLoading = false
   $(window).scroll(function () {
+    $('.blur-background').find('main').css({
+      transform: `translateY(-${$(window).scrollTop()}px)`,
+      '-webkit-transform': `translateY(-${$(window).scrollTop()}px)`
+    })
     if (!isLoading) {
       if ($(window).scrollTop() >= $(document).height() - $(window).height() - 1500) {
         isLoading = true
